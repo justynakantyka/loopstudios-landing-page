dae51a797ad78b9938277e3e950b5f04a5fcda03
justkantyka <just.kantyka@gmail.com>
2022-06-27 21:59:18 +0200

    Change backround for opened hamburger menu

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
   const navbarToggler = document.querySelector(".top-section .navbar-toggler");
 
   navbarToggler.addEventListener("click", () => {
-    console.log("OTWARTE");
-    const img = document.querySelector(".top-section .navbar-dark .navbar-toggler-icon").style.backgroundImage;
-    console.log(img);
+    if(navbarToggler.getAttribute("aria-expanded")==="true"){
+      console.log("OTWARTE");
+      document.querySelector(".top-section").style.background = "black";
+    } else {
+        document.querySelector(".top-section").style.background = "url('./images/mobile/image-hero.jpg') no-repeat center center";
+        document.querySelector(".top-section").style.backgroundSize = "cover";
+    }
   });

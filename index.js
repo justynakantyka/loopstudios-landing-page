55c3780e6e7c705619fcd1e603ae625a514e0ad9
justkantyka <just.kantyka@gmail.com>
2022-07-04 21:38:36 +0200

    Remove h1 from mobile second view

--- index.js
+++ index.js
@@ -4,10 +4,11 @@
     if(navbarToggler.getAttribute("aria-expanded")==="true"){
       console.log("OTWARTE");
       document.querySelector(".top-section").style.background = "black";
-      document.querySelector(".top-section").style.background = "black";
+      document.querySelector(".top-section h1").style.display = "none";
       document.querySelector(".top-section .navbar-dark .navbar-toggler-icon").style.backgroundImage = "url('./images/icon-close.svg')";
       document.querySelector(".top-section .navbar-dark .navbar-toggler-icon").style.width = "1.125rem";
     } else {
+        document.querySelector(".top-section h1").style.display = "block";
         document.querySelector(".top-section").style.background = "url('./images/mobile/image-hero.jpg') no-repeat center center";
         document.querySelector(".top-section").style.backgroundSize = "cover";
         document.querySelector(".top-section .navbar-dark .navbar-toggler-icon").style.backgroundImage = "url('./images/icon-hamburger.svg')";
